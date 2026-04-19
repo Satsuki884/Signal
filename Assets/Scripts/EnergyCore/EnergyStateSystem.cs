42f72cd3ba3278eba7a1375e42203d304b3b0ea2
Satsuki884 <stasyaklim884@gmail.com>
2026-04-19 15:03:21 +0300

    added blinking on energy low

--- Assets/Scripts/EnergyCore/EnergyStateSystem.cs
+++ Assets/Scripts/EnergyCore/EnergyStateSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,6 +10,13 @@ public class EnergyStateSystem : MonoBehaviour
     [SerializeField] private Slider _energyValueSlider;
     [SerializeField] private TMP_Text _energyValueText;
     [SerializeField] private TMP_Text _energyReduseSpeedText;
+    [SerializeField] private Color _energyFullColor = Color.white;
+    [SerializeField] private Color _energySmallColor = Color.red;
+    [SerializeField] private float _lowEnergyThreshold = 25.99f;
+    [SerializeField] private float _blinkSpeed = 2f;
+
+    private Coroutine _blinkCoroutine;
+    private bool _isBlinking = false;
 
     [Range(0f, 100f)]
     public float EnergyValue = 100f;
@@ -23,10 +31,10 @@ public class EnergyStateSystem : MonoBehaviour
     {
         Instance = this;
     }
-
     private void Update()
     {
         HandleEnergyIncrease();
+        HandleLowEnergyBlink();
     }
 
     public void AddEnergy(float amount)
@@ -38,6 +46,41 @@ public class EnergyStateSystem : MonoBehaviour
         _energyValueSlider.value = EnergyValue;
         _energyValueText.text = EnergyValue.ToString("F0");
 
+    }
+
+    private void HandleLowEnergyBlink()
+    {
+        if (EnergyValue <= _lowEnergyThreshold)
+        {
+            if (!_isBlinking)
+            {
+                _blinkCoroutine = StartCoroutine(BlinkEnergyText());
+                _isBlinking = true;
+            }
+        }
+        else
+        {
+            if (_isBlinking)
+            {
+                StopCoroutine(_blinkCoroutine);
+                _isBlinking = false;
+
+                // 🔥 повертаємо нормальний стан
+                _energyValueText.color = _energyFullColor;
+            }
+        }
+    }
+
+    private IEnumerator BlinkEnergyText()
+    {
+        while (true)
+        {
+            _energyValueText.color = _energySmallColor;
+            yield return new WaitForSeconds(1f / _blinkSpeed);
+
+            _energyValueText.color = _energyFullColor;
+            yield return new WaitForSeconds(1f / _blinkSpeed);
+        }
     }
 
     public void ReduceEnergy(float amount)
